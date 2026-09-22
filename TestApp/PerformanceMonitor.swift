0f3c885e191bb5ad3846fa5c3ea6715215df5289
Jinxuan Liao <liaojinxuan666@gmail.com>
2026-09-22 19:40:44 +0800

    更新 PerformanceMonitor.swift

--- TestApp/PerformanceMonitor.swift
+++ TestApp/PerformanceMonitor.swift
@@ -7,34 +7,34 @@ class PerformanceMonitor: ObservableObject {
     
     @Published var fps: Double = 0
     @Published var frameTimeMs: Double = 0
-    @Published var gpuTimeMs: Double = 0
     @Published var triangleCount: Int = 0
     
     private var lastTimestamp: CFTimeInterval = 0
     private var frameCount: Int = 0
     private var accumulatedTime: Double = 0
-    private var accumulatedGpu: Double = 0
     
-    func markFrame(gpuTime: Double) {
+    func markFrame() {
         let now = CACurrentMediaTime()
-        if lastTimestamp == 0 { lastTimestamp = now; return }
+        if lastTimestamp == 0 {
+            lastTimestamp = now
+            return
+        }
         
         let dt = now - lastTimestamp
         lastTimestamp = now
         
         frameCount += 1
         accumulatedTime += dt
-        accumulatedGpu += gpuTime
         
-        if accumulatedTime >= 1.0 {
+        if accumulatedTime >= 0.5 {
+            let fpsValue = Double(frameCount) / accumulatedTime
+            let frameMs = (accumulatedTime / Double(frameCount)) * 1000
             DispatchQueue.main.async {
-                self.fps = Double(self.frameCount) / self.accumulatedTime
-                self.frameTimeMs = (self.accumulatedTime / Double(self.frameCount)) * 1000
-                self.gpuTimeMs = (self.accumulatedGpu / Double(self.frameCount)) * 1000
+                self.fps = fpsValue
+                self.frameTimeMs = frameMs
             }
             frameCount = 0
             accumulatedTime = 0
-            accumulatedGpu = 0
         }
     }
     
