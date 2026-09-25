1d30dacdd76a1abd2257e9074bb1a63d92704f96
Jinxuan Liao <liaojinxuan666@gmail.com>
2026-09-25 18:58:50 +0800

    更新 ContentView.swift

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -192,7 +192,7 @@ struct MetalView: UIViewRepresentable {
             gal.setTransform(matrix: matrixArray)
 
             var modified = middleVerts
-            for i in stride(from: 1, to: modified.count, by: 12) { modified[i] = 5.0 }
+            for i in stride(from: 1, to: modified.count, by: 12) { modified[i] += 1.0 }
                 gal.updateVertexBuffer(meshRanges[1].vbo, data: modified, offset: 0)
 
             for mesh in meshRanges {
